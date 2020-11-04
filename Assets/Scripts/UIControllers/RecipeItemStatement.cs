797d98cbbef92c0850006a6bcba8d4bdaabe5fe3
KuzeyKurt <japarov.s.i20@gmail.com>
2020-11-04 20:56:47 +0300

    Added Recipe Panel

---
+++ Assets/Scripts/UIControllers/RecipeItemStatement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecipeItemStatement : MonoBehaviour
+{
+    private OpenMenu openMenu;
+
+
+    void Awake()
+    {
+        openMenu = FindObjectOfType<OpenMenu>();
+    }
+
+    public void OpenRecipeDetails()
+    {
+
+    }
+}
