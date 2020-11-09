9921af4f1dfa0720862a676bc353e51eba84d553
newL <seydametov.e.i.2.16@gmail.com>
2020-11-09 21:04:30 +0300

    drag UI windows

---
+++ Assets/Scripts/UIControllers/DragWindow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragWindow : MonoBehaviour, IDragHandler, IPointerDownHandler
+{
+    [SerializeField] private RectTransform dragRectTransform;
+    [SerializeField] private Canvas canvas;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        dragRectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        dragRectTransform.SetAsLastSibling();
+    }
+}
